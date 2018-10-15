aea4ebe4c3f7e0e9bcfd5dba762a29a81cdffd3a
Benjamin Pearson <bpearson@arklegaudit.gov>
2018-10-15 17:03:28 -0500

    remove code

--- popup.js
+++ popup.js
@@ -18,7 +18,7 @@ function view() {
         at the end of every line. That's called an escape character, which says the character after it isn't part
         of the string. The character after each backslash is a newline, so the actual string we end up creating
         doesn't have any Enters. You should also notice a backslash before quotes \" \". We have to escape
-        quotes to prevent confusion about where the string ends. 
+        quotes to prevent confusion about where the string ends.
     */
     var script = "function createXHR()\
     {\
@@ -42,125 +42,7 @@ function view() {
             }\
         return request;\
     }\
-    function getBody(content)\
-    {\
-       test = content.toLowerCase();\
-       var x = test.indexOf('<table class=\"table\">');\
-       if(x == -1) return \"\";\
-\
-       x = test.indexOf(\">\", x);\
-       if(x == -1) return \"\";\
-\
-       var y = test.lastIndexOf(\"</table>\");\
-       if(y == -1) y = test.lastIndexOf(\"</html>\");\
-       if(y == -1) y = content.length;\
-\
-       return content.slice(x+1, y);\
-    }\
-    function getHtml(content)\
-    {\
-       test = content.toLowerCase();\
-       var x = test.indexOf('<html lang=\"en\">');\
-       if(x == -1) return \"\";\
-\
-       x = test.indexOf(\">\", x);\
-       if(x == -1) return \"\";\
-\
-       var y = test.lastIndexOf(\"</html>\");\
-       if(y == -1) y = content.length;\
-\
-       return content.slice(x+1, y);\
-    }\
-    function loadHTML(url, fun, storage, param)\
-    {\
-    	var xhr = createXHR();\
-    	xhr.onreadystatechange=function()\
-    	{\
-    		if(xhr.readyState == 4)\
-    		{\
-    			{\
-    				storage.innerHTML = \"<table class='table col-sm-1'>\" + getBody(xhr.responseText) + \"</table>\";\
-    				fun(storage, param);\
-    			}\
-    		}\
-    	};\
-\
-    	xhr.open(\"GET\", url , false);\
-    	xhr.send(null);\
-\
-    }\
-	function processHTML(temp, target)\
-	{\
-		target.innerHTML = temp.innerHTML;\
-	}\
-\
-	function loadWholePage(url,appendix)\
-	{\
-		var y = document.getElementById(\"storage\"+appendix);\
-		var x = document.getElementById(\"displayed\"+appendix);\
-		loadHTML(url, processHTML, x, y);\
-	}\
-	function processByDOM(responseHTML, target)\
-	{\
-		target.innerHTML = \"Extracted by id:<br />\";\
-		var message = responseHTML.getElementsByTagName(\"div\").item(1).innerHTML;\
-\
-		target.innerHTML += message;\
-\
-		target.innerHTML += \"<br />Extracted by name:<br />\";\
-\
-		message = responseHTML.getElementsByTagName(\"form\").item(0);\
-		target.innerHTML += message.dyn.value;\
-	}\
-\
-	function accessByDOM(url)\
-	{\
-		var responseHTML = document.getElementsById(\"storage\");\
-		var y = document.getElementById(\"displayed\");\
-		loadHTML(url, processByDOM, responseHTML, y);\
-	}\
-    var columns = document.getElementsByTagName(\"td\");\
-    for(var i = 0;i<columns.length;i++){\
-    	columns[i].className = \"column\";\
-    }\
-    function insertAfter(referenceNode, newNode) {\
-        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);\
-    }\
-    for(var i = 0;i < columns.length;i++){\
-    	var links = columns[i].getElementsByTagName(\"a\");\
-    	for(var j = 0;j < links.length;j++){\
-            var storage = document.createElement(\"div\");\
-    		var displayed = document.createElement(\"div\");\
-    		var appendix = i+\"_\"+j;\
-            storage.id = \"storage\"+appendix;\
-    		storage.style.display = \"none\";\
-    		displayed.id = \"displayed\"+appendix;\
-            var a = links[j];\
-            insertAfter(a, storage);\
-    		insertAfter(a, displayed);\
-    		loadWholePage('https://local.arkansas.gov/local.php?agency='+a.textContent,appendix);\
-        }\
-    }\
-    document.getElementById('intro').remove();\
-    document.getElementsByClassName('col-md-6')[0].remove();\
-    document.getElementsByClassName('col-md-6')[0].className='col-md-12';\
-    var remaining = true;\
-    while(remaining){\
-    remaining = false;\
-    var columns = document.getElementsByClassName(\"column\");\
-        for(var i = 0;i < columns.length;i++){\
-            var links = columns[i].getElementsByTagName(\"div\");\
-            for(var j = 0;j < links.length;j++){\
-                var rows = links[j].getElementsByTagName(\"tr\");\
-                for(var k=0;k<rows.length;k++){\
-                    if(!rows[k].textContent.includes(\"(Vacant)\")){\
-                        rows[k].remove();\
-    					remaining = true;\
-                    }\
-                }\
-            }\
-        }\
-    }";
+    ";
     /* It's common for programmers to use libraries of code other people wrote.
         We're using the "chrome" library, which includes other libraries such as "tabs".
         "tabs" has a function inside called "executeScript", which we're using
